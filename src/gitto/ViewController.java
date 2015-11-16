795eafa01c4388d7acccc49bfa32012a2dd34299
Akio Yasui <akkyie01@gmail.com>
2015-11-16 16:11:47 +0900

    ファイルツリーの表示を実装

--- src/gitto/ViewController.java
+++ src/gitto/ViewController.java
@@ -6,6 +6,7 @@ import java.util.ArrayList;
 import java.util.List;
 import java.util.ResourceBundle;
 
+import javafx.beans.property.ReadOnlyObjectWrapper;
 import javafx.collections.FXCollections;
 import javafx.collections.ObservableList;
 import javafx.event.ActionEvent;
@@ -13,6 +14,8 @@ import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.scene.control.*;
 import javafx.scene.control.cell.PropertyValueFactory;
+
+import javafx.scene.control.cell.TreeItemPropertyValueFactory;
 import org.eclipse.jgit.api.Git;
 import org.eclipse.jgit.lib.Constants;
 import org.eclipse.jgit.lib.ObjectId;
@@ -23,10 +26,15 @@ import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
 public class ViewController implements Initializable {
 
     @FXML private TextField textField;
+
+    // commit log
     @FXML private TableView<Commit> tableView;
     @FXML private TableColumn<Commit, String> commitIDColumn;
     @FXML private TableColumn<Commit, String> messageColumn;
 
+    // file tree
+    @FXML private TreeTableView<File> fileTreeTableView;
+    @FXML private TreeTableColumn<File, String> filenameColumn;
     private Git git;
 
     private ObservableList<Commit> commits;
@@ -38,16 +46,20 @@ public class ViewController implements Initializable {
     @Override public void initialize(URL location, ResourceBundle resources) {
         this.commitIDColumn.setCellValueFactory(new PropertyValueFactory<>("commitID"));
         this.messageColumn.setCellValueFactory(new PropertyValueFactory<>("message"));
+        this.filenameColumn.setCellValueFactory((file) -> {
+            return new ReadOnlyObjectWrapper<>(file.getValue().getValue().getName());
+        });
         try {
-            this.initializeGit("./");
+            this.loadFromPath("./");
         } catch (Exception error) {
             error.printStackTrace();
         }
     }
 
-    private void initializeGit(String location) throws Exception {
+    private void loadFromPath(String location) throws Exception {
         FileRepositoryBuilder builder = new FileRepositoryBuilder();
-        Repository repository = builder.setGitDir(new File(location + Constants.DOT_GIT)).readEnvironment().findGitDir().build();
+        File gitDir = new File(location + Constants.DOT_GIT);
+        Repository repository = builder.setGitDir(gitDir).readEnvironment().findGitDir().build();
         this.git = new Git(repository);
 
         Integer count = 10;
@@ -61,16 +73,64 @@ public class ViewController implements Initializable {
             commits.add(commit);
         }
         this.setCommits(commits);
+
+        this.fileTreeTableView.setRoot(createNode(gitDir));
     }
 
-    @FXML private void loadGitFromPath(ActionEvent event) {
+    @FXML private void loadButtonDidPress(ActionEvent event) {
         String path = this.textField.getText();
         System.out.println(path);
         try {
-            this.initializeGit(path);
+            this.loadFromPath(path);
         } catch (Exception error) {
             error.printStackTrace();
         }
+    }
+
+    // utility function to build file tree
+    // https://gist.github.com/james-d/11177668
+
+    private TreeItem<File> createNode(final File f) {
+        return new TreeItem<File>(f) {
+            private boolean isLeaf;
+            private boolean isFirstTimeChildren = true;
+            private boolean isFirstTimeLeaf = true;
+
+            @Override
+            public ObservableList<TreeItem<File>> getChildren() {
+                if (isFirstTimeChildren) {
+                    isFirstTimeChildren = false;
+                    super.getChildren().setAll(buildChildren(this));
+                }
+                return super.getChildren();
+            }
+
+            @Override
+            public boolean isLeaf() {
+                if (isFirstTimeLeaf) {
+                    isFirstTimeLeaf = false;
+                    File f = getValue();
+                    isLeaf = f.isFile();
+                }
+                return isLeaf;
+            }
+        };
+    }
+
+    private ObservableList<TreeItem<File>> buildChildren(TreeItem<File> TreeItem) {
+        File f = TreeItem.getValue();
+        if (f == null || !f.isDirectory()) {
+            return FXCollections.emptyObservableList();
+        }
+        File[] files = f.listFiles();
+        if (files == null) {
+            return FXCollections.emptyObservableList();
+        }
+        ObservableList<TreeItem<File>> children = FXCollections.observableArrayList();
+        for (File childFile: files) {
+            children.add(createNode(childFile));
+        }
+        return children;
     }
 
 }
