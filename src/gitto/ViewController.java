279cbb5413550e5cffed681d6d2ebd8cbc3ed95b
Akio Yasui <akkyie01@gmail.com>
2016-01-24 01:43:58 +0900

    政教分離

--- src/gitto/ViewController.java
+++ src/gitto/ViewController.java
@@ -1,9 +1,7 @@
 package gitto;
 
-import java.awt.event.ActionEvent;
 import java.io.File;
 import java.net.URL;
-import java.nio.file.Paths;
 import java.util.ResourceBundle;
 
 import javafx.fxml.FXML;
@@ -13,28 +11,22 @@ import javafx.scene.input.DragEvent;
 import javafx.scene.input.Dragboard;
 import javafx.scene.input.TransferMode;
 import javafx.scene.layout.Pane;
-import org.eclipse.jgit.api.Git;
-import org.eclipse.jgit.lib.Constants;
-import org.eclipse.jgit.lib.Repository;
-import org.eclipse.jgit.storage.file.FileRepositoryBuilder;
-import org.eclipse.jgit.api.errors.GitAPIException;
-import org.eclipse.jgit.util.SystemReader;
-
-import java.util.Date;
 
 public class ViewController implements Initializable {
 
     @FXML private TextField textField;
     @FXML private Pane      dropPane;
 
-    private Git git;
+    private GitController controller;
 
     @FXML
     private void commitButtonDidPush() {
-        commitGit();
+        controller.commit();
     }
 
     @Override public void initialize(URL location, ResourceBundle resources) {
+        controller = GitController.sharedController();
+
         dropPane.addEventHandler(DragEvent.DRAG_OVER, (DragEvent event) -> {
             Dragboard db = event.getDragboard();
             if (db.hasFiles()) {
@@ -46,83 +38,17 @@ public class ViewController implements Initializable {
         dropPane.addEventHandler(DragEvent.DRAG_DROPPED, (DragEvent event) -> {
             Dragboard db = event.getDragboard();
             db.getFiles().stream().findFirst().ifPresent((File file) -> {
-                loadGit(file);
-                addFile(file);
+                File dir = file.isDirectory() ? file : file.getParentFile();
+                File repository = controller.findRepository(dir);
+                if (repository == null) {
+                    controller.init(dir);
+                }
+                controller.add(file);
+
+                this.textField.setText(controller.getCurrentRepository().getParent() + "/");
             });
             event.setDropCompleted(true);
         });
     }
 
-    private void loadGit(File file) {
-        System.out.println("DnD:" + file.getAbsolutePath());
-
-        // .gitのあるディレクトリ
-        File dir = file.isDirectory() ? file : file.getParentFile();
-
-        FileRepositoryBuilder builder = new FileRepositoryBuilder();
-
-        // .gitのパス
-        File gitPath = Paths.get(dir.getPath(), Constants.DOT_GIT).toFile();
-        builder.findGitDir(gitPath);
-
-        System.out.println(gitPath.getAbsolutePath());
-
-        try {
-            if (builder.getGitDir() == null) { // .gitがない
-                this.git = new Git(FileRepositoryBuilder.create(gitPath));
-                gitInit(dir);
-            } else { // .gitがある
-                this.git = new Git(builder.build());
-            }
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            return;
-        }
-        this.textField.setText(dir.getPath());
-    }
-
-    private void initTargetGit(){
-        this.git = null;
-    }
-
-    private void gitInit(File dir){
-        try {
-            this.git.init().setDirectory(dir).setBare(false).call();
-            System.out.println("git init");
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            return;
-        }
-    }
-
-    private void addFile(File file) {
-        try {
-            // .gitのあるディレクトリからの相対パスを生成
-            String Path;
-            if (file.isFile()){
-                Path = file.getAbsolutePath().replaceAll(this.git.getRepository().getDirectory().getParent()+"/","");
-            }else if(file.getAbsolutePath().equals(this.git.getRepository().getDirectory().getParent())){// カレントディレクトリ
-                Path = ".";
-            }else{ // file.isDirectory() == true
-                Path = file.getAbsolutePath().replaceAll(this.git.getRepository().getDirectory().getParent(),"");
-            }
-            System.out.println("git add " + Path);
-            this.git.add().addFilepattern(Path).call();
-
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            return;
-        }
-    }
-
-    private void commitGit(){
-        try{
-            String commitMessage = new Date().toString();
-            this.git.commit().setMessage(commitMessage).call();
-            System.out.println("git commit -m "+ commitMessage);
-        } catch (Exception exception) {
-            exception.printStackTrace();
-            return;
-        }
-    }
 }
